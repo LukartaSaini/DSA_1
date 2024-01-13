528bfe56ba4457c476866a6ffe3caf533ff51944
Lukarta Saini <104339528+LukartaSaini@users.noreply.github.com>
2024-01-13 01:14:40 -0800

    Add files via upload

---
+++ Fibonic Series.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        {
+            int a=0,b=1;
+            int c;
+            for(int i=1;i<=9;i++){
+               System.out.print(a+" ");  
+                c=a+b; a=b; b=c;}
+        }
+    }
+}
